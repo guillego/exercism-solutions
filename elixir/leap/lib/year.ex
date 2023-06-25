b328ed7ecc641791d368c7f92d6708067de4da7b
Guillermo Galan <guille@clutchapp.io>
2023-06-25 10:46:47 +0200

    Solve leap year exercise

--- elixir/leap/lib/year.ex
+++ elixir/leap/lib/year.ex
@@ -10,5 +10,16 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
+    rem400 = rem(year, 400)
+    rem100 = rem(year, 100)
+    rem4 = rem(year, 4)
+
+    cond do
+      rem400 == 0 -> true
+      rem100 == 0 -> false
+      rem4 == 0 -> true
+      true -> false
+    end
+
   end
 end
