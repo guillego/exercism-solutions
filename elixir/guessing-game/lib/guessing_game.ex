04762c191c2114e5edfa91564f0b3fcea2113e52
Guillermo Galan <guille@clutchapp.io>
2023-06-25 10:30:56 +0200

    Solve guessing game exercise

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,26 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+  def compare(secret_number, :no_guess) do
+    compare(secret_number)
+  end
+
+  def compare(secret_number, guess) when secret_number == guess do
+    "Correct"
+  end
+
+  def compare(secret_number, guess) when guess + 1 == secret_number or guess - 1 == secret_number do
+    "So close"
+  end
+
+  def compare(secret_number, guess) when guess > secret_number do
+    "Too high"
+  end
+
+  def compare(secret_number, guess) when guess < secret_number do
+    "Too low"
+  end
+
+
+  def compare(secret_number) do
+    "Make a guess"
   end
 end
