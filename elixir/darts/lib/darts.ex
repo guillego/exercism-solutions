eaf447cd74bb1418d00613e246d5b81f1493618f
Guillermo Galan <guille@clutchapp.io>
2023-06-25 10:54:23 +0200

    Solve darts exercise

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,16 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    rad = radius(x,y)
+    cond do
+      rad <= 1 -> 10
+      rad <= 5 -> 5
+      rad <= 10 -> 1
+      true -> 0
+    end
+  end
+
+  defp radius(x,y) do
+    :math.sqrt(:math.pow(x,2) + :math.pow(y,2))
   end
 end
